1251d83215a04cf152c729442c102830416fddba
kkpagaev <kkpagaev@gmail.com>
2023-06-10 03:42:33 +0300

    update

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,7 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+
 if status --is-login
     set -gx PATH $PATH ~/linux/bin
 end
@@ -16,13 +17,20 @@ function sudo --description "Replacement for Bash 'sudo !!' command to run last 
     end
 end
 . ~/.bash_aliases
+#source /home/kkpagaev/.gvm/scripts/gvm
 export  PATH="$HOME/.local/bin:$PATH"
 export  PATH="/home/kkpagaev/bin:$PATH"
 export  PATH="/home/kkpagaev/go/bin:$PATH"
 export  PATH="/home/kkpagaev/.ghcup/bin:$PATH"
+export  PATH="/home/kkpagaev/.sdkman/candidates/java/current/bin:$PATH"
+#export  PATH="/home/kkpagaev/.rvm/bin:$PATH"
+export  PATH="$PATH:/home/kkpagaev/.rvm/rubies/default/bin"
 set -gx PATH "$HOME/.cargo/bin" $PATH;
 
+
 export PATH="/usr/local/go/bin:$PATH"
+
+export PATH="/home/kkpagaev/.config/emacs/bin:$PATH"
 
 export PATH="/home/kkpagaev/.local/share/nvim/mason/bin:$PATH"
 # pnpm
@@ -30,12 +38,41 @@ set -gx PNPM_HOME "/home/kkpagaev/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 set -gx PATH "/usr/local/" $PATH
 # pnpm end
+set -gx BROWSER "google-chrome-stable"
 
 export PACKER_PATH="/home/kkpagaev/.local/share/nvim/site/pack/packer/start"
+
+if status --is-interactive
+        zoxide init fish | source
+end
 
 fish_add_path /home/kkpagaev/.spicetify
 
 #set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kkpagaev/.ghcup/bin $PATH # ghcup-env
 #set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kkpagaev/.ghcup/bin $PATH # ghcup-env
 
-set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kkpagaev/.ghcup/bin $PATH # ghcup-env
+#set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/kkpagaev/.ghcup/bin $PATH # ghcup-env
+
+set -g fish_greeting
+
+export VISUAL=nvim
+export EDITOR="$VISUAL"
+
+set -U fish_user_paths ~/dotfiles/scripts $fish_user_paths
+
+set --universal nvm_default_version v18.16.0
+
+function bang  --description "Replacement for Bash 'sudo !!' command to run last command using sudo."
+    echo $history[1]
+    eval command $history[1]
+end
+
+  # ~/.tmux/plugins
+fish_add_path $HOME/.tmux/plugins/t-smart-tmux-session-manager/bin
+# ~/.config/tmux/plugins
+fish_add_path $HOME/.config/tmux/plugins/t-smart-tmux-session-manager/bin
+
+source ~/.asdf/asdf.fish
+
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /home/kkpagaev/.ghcup/bin # ghcup-env
